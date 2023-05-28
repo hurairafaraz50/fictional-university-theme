1c8b63141e0ac42496af18af0b3073f95312928e
Huraira faraz khan <hurairafaraz50@gmail.com>
2023-05-28 11:29:38 +0500

    added parent and child pages

--- page.php
+++ page.php
@@ -17,25 +17,27 @@ while(have_posts())
 
     <div class="container container--narrow page-section">
       <?php
-        if(wp_get_post_parent_id(get_the_ID)){ ?>
+        $theParent =wp_get_post_parent_id(get_the_ID());
+        if ( $theParent ) { ?>
         <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
-          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main"><?php the_title() ?></span>
+        <a class="metabox__blog-home-link" href="<?php echo get_permalink( $theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title( $theParent); ?></a> <span class="metabox__main"><?php the_title() ?></span>
         </p>
         </div>
       <?php
         }
       ?>
       
-      <!--
+    
       <div class="page-links">
         <h2 class="page-links__title"><a href="#">About Us</a></h2>
         <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
+            <?php
+               
+            ?>
         </ul>
       </div>
-    -->
+    
 
       <div class="generic-content">
         <?php the_content(); ?> 
